1de876cc70c398deae563f6a337a4fef855b6a3d
sayalikohad <138372300+sayalikohad@users.noreply.github.com>
2023-10-19 20:31:48 +0530

    add home.js

--- Home.js
+++ Home.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css'
 
 function Home() {
+    const [city,setCity] = useState('Nagpur');
+    const [temperature, setTemperature] = useState(0)
+    
     return (
         <div>
-            <h1 className='app-title'>Weather App</h1>
+            <h1 className='app-title'>Weather For {city}</h1>
 
-            <input className='search-bar' type='text' placeholder='Enter City...'/>
+            <input
+                className='search-bar'
+                type='text'
+                placeholder='Enter City...'
+                value={city}
+                onChange={(e)=>{
+                    setCity(e.target.value)
+                }}
+            />
 
-            <h1 className='temperature-text'>Temperature: </h1>
+            <h1 className='temperature-text'>Temperature: {temperature} °C</h1>
         </div>
     )
 }
