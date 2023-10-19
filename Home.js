af00cb77d23a82e3a0d2a8976d9d890fae63ab08
sayalikohad <138372300+sayalikohad@users.noreply.github.com>
2023-10-19 20:04:27 +0530

    add Temperature

--- Home.js
+++ Home.js
@@ -7,6 +7,8 @@ function Home() {
             <h1 className='app-title'>Weather App</h1>
 
             <input className='search-bar' type='text' placeholder='Enter City...'/>
+
+            <h1 className='temperature-text'>Temperature: </h1>
         </div>
     )
 }
