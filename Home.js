b651f661fac80c2a4823e2cbff0a633146f89220
sayalikohad <138372300+sayalikohad@users.noreply.github.com>
2023-10-20 15:40:58 +0530

    add home.js

--- Home.js
+++ Home.js
@@ -1,10 +1,29 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 import './Home.css'
 
 function Home() {
     const [city,setCity] = useState('Nagpur');
     const [temperature, setTemperature] = useState(0)
+    const [message, setMessage] = useState('')
     
+    async function loadweatherInfo(){
+        try{
+            const response = await axios.get('https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f652964084c552e8c0492237a3fabd9c')
+
+            setTemperature((response.data.main.temp -273).toFixed(2))
+            setMessage('city Found')
+        }
+        catch(err){
+           setTemperature(0)
+           setMessage('city Not Found')
+        }
+    }
+
+    useEffect(()=>{
+        loadweatherInfo()
+    }, [city])
+
     return (
         <div>
             <h1 className='app-title'>Weather For {city}</h1>
@@ -18,8 +37,10 @@ function Home() {
                     setCity(e.target.value)
                 }}
             />
+              <p>{message}</p>
 
             <h1 className='temperature-text'>Temperature: {temperature} °C</h1>
+          
         </div>
     )
 }
